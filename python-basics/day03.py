c875f1bb173f0bd4891238694c762f045dc0846e
詹增杰 <93826191@qq.com>
2026-09-23 10:32:12 +0800

    Day 3: identifiers and operators

---
+++ python-basics/day03.py
@@ -0,0 +1,12 @@
+#//是整除， %是取余数，**是指数
+#c+=1即c=c+1,其他同理
+a=float(input("请输入一个数字"))
+b=float(input("请输入一个数字"))
+#input() 得到的内容默认是字符串,所以需要转为浮点数才能计算
+print("a+b=",a+b)
+print("a-b=",a-b)
+print("a*b=",a*b)
+print("a/b=",a/b)
+print("a//b=",a//b)
+print("a**b=",a**b)
+print("a%b=",a%b)
